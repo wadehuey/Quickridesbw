a9ae79bd33188d0e8cda0198aca3964321d7cfc8
wadehuey <92360022+wadehuey@users.noreply.github.com>
2026-08-27 13:15:24 +0200

    Add live map screen

---
+++ quickridesbw/lib/screens/live_map_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class LiveMapScreen extends StatelessWidget{const LiveMapScreen({super.key});@override Widget build(BuildContext c)=>Scaffold(appBar:AppBar(title:const Text('Live map')),body:const Center(child:Text('QuickRidesBW live GPS map')));}
